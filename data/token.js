dd955130024add29ffc850360f1d84918aa43767
webber0928 <bird23074035@gmail.com>
2024-05-01 17:10:35 +0800

    資料先用 js 來取代

---
+++ data/token.js
@@ -0,0 +1,122 @@
+module.exports = {
+  1: {
+    token: 'admin-token',
+    username: '1'
+  },
+  2: {
+    token: 'admin-token',
+    username: '2'
+  },
+  3: {
+    token: 'admin-token',
+    username: '3'
+  },
+  4: {
+    token: 'admin-token',
+    username: '4'
+  },
+  5: {
+    token: 'admin-token',
+    username: '5'
+  },
+  6: {
+    token: 'admin-token',
+    username: '6'
+  },
+  7: {
+    token: 'admin-token',
+    username: '7'
+  },
+  8: {
+    token: 'admin-token',
+    username: '8'
+  },
+  9: {
+    token: 'admin-token',
+    username: '9'
+  },
+  10: {
+    token: 'admin-token',
+    username: '10'
+  },
+  11: {
+    token: 'admin-token',
+    username: '11'
+  },
+  12: {
+    token: 'admin-token',
+    username: '12'
+  },
+  13: {
+    token: 'admin-token',
+    username: '13'
+  },
+  14: {
+    token: 'admin-token',
+    username: '14'
+  },
+  15: {
+    token: 'admin-token',
+    username: '15'
+  },
+  16: {
+    token: 'admin-token',
+    username: '16'
+  },
+  17: {
+    token: 'admin-token',
+    username: '17'
+  },
+  18: {
+    token: 'admin-token',
+    username: '18'
+  },
+  19: {
+    token: 'admin-token',
+    username: '19'
+  },
+  20: {
+    token: 'admin-token',
+    username: '20'
+  },
+  21: {
+    token: 'admin-token',
+    username: '21'
+  },
+  22: {
+    token: 'admin-token',
+    username: '22'
+  },
+  23: {
+    token: 'admin-token',
+    username: '23'
+  },
+  24: {
+    token: 'admin-token',
+    username: '24'
+  },
+  25: {
+    token: 'admin-token',
+    username: '25'
+  },
+  26: {
+    token: 'admin-token',
+    username: '26'
+  },
+  27: {
+    token: 'admin-token',
+    username: '27'
+  },
+  28: {
+    token: 'admin-token',
+    username: '28'
+  },
+  29: {
+    token: 'admin-token',
+    username: '29'
+  },
+  30: {
+    token: 'admin-token',
+    username: '30'
+  }
+}
