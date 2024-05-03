9e1a51df672b75a933fcad60cb6f0a78fac00a12
webber0928 <bird23074035@gmail.com>
2024-05-03 16:56:27 +0800

    Change: update

--- migrations/20240426072920-create_teachers_table.js
+++ migrations/20240426072920-create_teachers_table.js
@@ -1,22 +1,26 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async(queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      user_no: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      grade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async(queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
   }
-};
+}
