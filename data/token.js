65ddcd93fb4a641ba711ac2b32dce287df5d9241
webber0928 <bird23074035@gmail.com>
2024-05-04 23:53:22 +0800

    修改 pomot

--- data/token.js
+++ data/token.js
@@ -1,122 +1,418 @@
 module.exports = {
-  1: {
+  5101: {
     token: 'admin-token',
-    username: '1'
+    username: '5101'
   },
-  2: {
+  5102: {
     token: 'admin-token',
-    username: '2'
+    username: '5102'
   },
-  3: {
+  5103: {
     token: 'admin-token',
-    username: '3'
+    username: '5103'
   },
-  4: {
+  5104: {
     token: 'admin-token',
-    username: '4'
+    username: '5104'
   },
-  5: {
+  5105: {
     token: 'admin-token',
-    username: '5'
+    username: '5105'
   },
-  6: {
+  5106: {
     token: 'admin-token',
-    username: '6'
+    username: '5106'
   },
-  7: {
+  5107: {
     token: 'admin-token',
-    username: '7'
+    username: '5107'
   },
-  8: {
+  5108: {
     token: 'admin-token',
-    username: '8'
+    username: '5108'
   },
-  9: {
+  5109: {
     token: 'admin-token',
-    username: '9'
+    username: '5109'
   },
-  10: {
+  5110: {
     token: 'admin-token',
-    username: '10'
+    username: '5110'
   },
-  11: {
+  5111: {
     token: 'admin-token',
-    username: '11'
+    username: '5111'
   },
-  12: {
+  5112: {
     token: 'admin-token',
-    username: '12'
+    username: '5112'
   },
-  13: {
+  5113: {
     token: 'admin-token',
-    username: '13'
+    username: '5113'
   },
-  14: {
+  5114: {
     token: 'admin-token',
-    username: '14'
+    username: '5114'
   },
-  15: {
+  5115: {
     token: 'admin-token',
-    username: '15'
+    username: '5115'
   },
-  16: {
+  5116: {
     token: 'admin-token',
-    username: '16'
+    username: '5116'
   },
-  17: {
+  5117: {
     token: 'admin-token',
-    username: '17'
+    username: '5117'
   },
-  18: {
+  5118: {
     token: 'admin-token',
-    username: '18'
+    username: '5118'
   },
-  19: {
+  5119: {
     token: 'admin-token',
-    username: '19'
+    username: '5119'
   },
-  20: {
+  5120: {
     token: 'admin-token',
-    username: '20'
+    username: '5120'
   },
-  21: {
+  5121: {
     token: 'admin-token',
-    username: '21'
+    username: '5121'
   },
-  22: {
+  5122: {
     token: 'admin-token',
-    username: '22'
+    username: '5122'
   },
-  23: {
+  5123: {
     token: 'admin-token',
-    username: '23'
+    username: '5123'
   },
-  24: {
+  5124: {
     token: 'admin-token',
-    username: '24'
+    username: '5124'
   },
-  25: {
+  5125: {
     token: 'admin-token',
-    username: '25'
+    username: '5125'
   },
-  26: {
+  5199: {
     token: 'admin-token',
-    username: '26'
+    username: '5199'
   },
-  27: {
+  5201: {
     token: 'admin-token',
-    username: '27'
+    username: '5201'
   },
-  28: {
+  5202: {
     token: 'admin-token',
-    username: '28'
+    username: '5202'
   },
-  29: {
+  5203: {
     token: 'admin-token',
-    username: '29'
+    username: '5203'
   },
-  30: {
+  5204: {
     token: 'admin-token',
-    username: '30'
-  }
+    username: '5204'
+  },
+  5205: {
+    token: 'admin-token',
+    username: '5205'
+  },
+  5206: {
+    token: 'admin-token',
+    username: '5206'
+  },
+  5207: {
+    token: 'admin-token',
+    username: '5207'
+  },
+  5208: {
+    token: 'admin-token',
+    username: '5208'
+  },
+  5209: {
+    token: 'admin-token',
+    username: '5209'
+  },
+  5210: {
+    token: 'admin-token',
+    username: '5210'
+  },
+  5211: {
+    token: 'admin-token',
+    username: '5211'
+  },
+  5212: {
+    token: 'admin-token',
+    username: '5212'
+  },
+  5213: {
+    token: 'admin-token',
+    username: '5213'
+  },
+  5214: {
+    token: 'admin-token',
+    username: '5214'
+  },
+  5215: {
+    token: 'admin-token',
+    username: '5215'
+  },
+  5216: {
+    token: 'admin-token',
+    username: '5216'
+  },
+  5217: {
+    token: 'admin-token',
+    username: '5217'
+  },
+  5218: {
+    token: 'admin-token',
+    username: '5218'
+  },
+  5219: {
+    token: 'admin-token',
+    username: '5219'
+  },
+  5220: {
+    token: 'admin-token',
+    username: '5220'
+  },
+  5221: {
+    token: 'admin-token',
+    username: '5221'
+  },
+  5222: {
+    token: 'admin-token',
+    username: '5222'
+  },
+  5223: {
+    token: 'admin-token',
+    username: '5223'
+  },
+  5224: {
+    token: 'admin-token',
+    username: '5224'
+  },
+  5225: {
+    token: 'admin-token',
+    username: '5225'
+  },
+  5299: {
+    token: 'admin-token',
+    username: '5299'
+  },
+  5301: {
+    token: 'admin-token',
+    username: '5301'
+  },
+  5302: {
+    token: 'admin-token',
+    username: '5302'
+  },
+  5303: {
+    token: 'admin-token',
+    username: '5303'
+  },
+  5304: {
+    token: 'admin-token',
+    username: '5304'
+  },
+  5305: {
+    token: 'admin-token',
+    username: '5305'
+  },
+  5306: {
+    token: 'admin-token',
+    username: '5306'
+  },
+  5307: {
+    token: 'admin-token',
+    username: '5307'
+  },
+  5308: {
+    token: 'admin-token',
+    username: '5308'
+  },
+  5309: {
+    token: 'admin-token',
+    username: '5309'
+  },
+  5310: {
+    token: 'admin-token',
+    username: '5310'
+  },
+  5311: {
+    token: 'admin-token',
+    username: '5311'
+  },
+  5312: {
+    token: 'admin-token',
+    username: '5312'
+  },
+  5313: {
+    token: 'admin-token',
+    username: '5313'
+  },
+  5314: {
+    token: 'admin-token',
+    username: '5314'
+  },
+  5315: {
+    token: 'admin-token',
+    username: '5315'
+  },
+  5316: {
+    token: 'admin-token',
+    username: '5316'
+  },
+  5317: {
+    token: 'admin-token',
+    username: '5317'
+  },
+  5318: {
+    token: 'admin-token',
+    username: '5318'
+  },
+  5319: {
+    token: 'admin-token',
+    username: '5319'
+  },
+  5320: {
+    token: 'admin-token',
+    username: '5320'
+  },
+  5321: {
+    token: 'admin-token',
+    username: '5321'
+  },
+  5322: {
+    token: 'admin-token',
+    username: '5322'
+  },
+  5323: {
+    token: 'admin-token',
+    username: '5323'
+  },
+  5324: {
+    token: 'admin-token',
+    username: '5324'
+  },
+  5325: {
+    token: 'admin-token',
+    username: '5325'
+  },
+  5399: {
+    token: 'admin-token',
+    username: '5399'
+  },
+  5401: {
+    token: 'admin-token',
+    username: '5401'
+  },
+  5402: {
+    token: 'admin-token',
+    username: '5402'
+  },
+  5403: {
+    token: 'admin-token',
+    username: '5403'
+  },
+  5404: {
+    token: 'admin-token',
+    username: '5404'
+  },
+  5405: {
+    token: 'admin-token',
+    username: '5405'
+  },
+  5406: {
+    token: 'admin-token',
+    username: '5406'
+  },
+  5407: {
+    token: 'admin-token',
+    username: '5407'
+  },
+  5408: {
+    token: 'admin-token',
+    username: '5408'
+  },
+  5409: {
+    token: 'admin-token',
+    username: '5409'
+  },
+  5410: {
+    token: 'admin-token',
+    username: '5410'
+  },
+  5411: {
+    token: 'admin-token',
+    username: '5411'
+  },
+  5412: {
+    token: 'admin-token',
+    username: '5412'
+  },
+  5413: {
+    token: 'admin-token',
+    username: '5413'
+  },
+  5414: {
+    token: 'admin-token',
+    username: '5414'
+  },
+  5415: {
+    token: 'admin-token',
+    username: '5415'
+  },
+  5416: {
+    token: 'admin-token',
+    username: '5416'
+  },
+  5417: {
+    token: 'admin-token',
+    username: '5417'
+  },
+  5418: {
+    token: 'admin-token',
+    username: '5418'
+  },
+  5419: {
+    token: 'admin-token',
+    username: '5419'
+  },
+  5420: {
+    token: 'admin-token',
+    username: '5420'
+  },
+  5421: {
+    token: 'admin-token',
+    username: '5421'
+  },
+  5422: {
+    token: 'admin-token',
+    username: '5422'
+  },
+  5423: {
+    token: 'admin-token',
+    username: '5423'
+  },
+  5424: {
+    token: 'admin-token',
+    username: '5424'
+  },
+  5425: {
+    token: 'admin-token',
+    username: '5425'
+  },
+  5499: {
+    token: 'admin-token',
+    username: '5499'
+  },
 }
