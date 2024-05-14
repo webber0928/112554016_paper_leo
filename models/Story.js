934a74f138606af1de635e904a593662a309b173
webber0928 <bird23074035@gmail.com>
2024-05-14 09:18:20 +0800

    Feature: add ranking and words to Story table

--- models/Story.js
+++ models/Story.js
@@ -8,6 +8,10 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true,
       primaryKey: true
     },
+    ranking: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: false
@@ -15,6 +19,14 @@ module.exports = (sequelize, DataTypes) => {
     content: {
       type: DataTypes.TEXT,
       allowNull: false
+    },
+    words: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      get: function() {
+        if (!this.getDataValue('words')) return null
+        return JSON.parse(this.getDataValue('words'))
+      }
     },
     deleted_at: {
       type: DataTypes.BIGINT,
