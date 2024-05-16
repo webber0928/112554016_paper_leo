3a490cab3613dd619b8a724c063d62ef69b25080
webber0928 <bird23074035@gmail.com>
2024-05-16 16:55:32 +0800

    Change: update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,19 +5,13 @@ module.exports = {
     instances: 1,
     script: 'npm',
     args: 'run dev',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    env: {
-      NODE_ENV: 'develop'
-    }
+    log_date_format: 'YYYY-MM-DD HH:mm:ss'
   }, {
     name: 'web-api',
     exec_mode: 'fork',
     instances: 1,
     script: 'npm',
     args: 'run api',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    env: {
-      NODE_ENV: 'develop'
-    }
+    log_date_format: 'YYYY-MM-DD HH:mm:ss'
   }]
 }
